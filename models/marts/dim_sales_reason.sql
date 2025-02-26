00284496c12b87015be434188d005c86b5271857
luizapllgrn@gmail.com <luizapllgrn@gmail.com>
2025-02-26 13:56:09 +0000

    adição da coluna sales_order_id à dim_sales_reason

--- models/marts/dim_sales_reason.sql
+++ models/marts/dim_sales_reason.sql
@@ -1,10 +1,12 @@
 with dim_sales_reason as (
 select
-    sales_reason_id as sales_reason_id,
-    sales_reason as sales_reason,
-    sales_reason_type as sales_reason_type,
+    sr.sales_reason_id as sales_reason_id,
+    sr.sales_reason as sales_reason,
+    sr.sales_reason_type as sales_reason_type,
+    so.sales_order_id
     
-from {{ ref('int_salesreason') }}
+from {{ ref('int_salesreason') }} sr
+left join {{ ref('int_salesorderheadersalesreason') }} so on sr.sales_reason_id = so.sales_reason_id
 )
 
 select * from dim_sales_reason
